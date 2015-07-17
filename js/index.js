538e419d17ef66e1b9325b0c0696bf822e1e795d
xdsking <xdsking@163.com>
2015-07-17 16:44:17 +0800

    ddd

--- js/index.js
+++ js/index.js
@@ -1,6 +1,19 @@
 /**
  * Created by xuds on 2015/7/6.
  */
+var showOperatePane=function(operatePane,flag){
+    $(".operatePane").css("width","0");
+    if(flag){
+        $("#videoPane").css("width","80%");
+        operatePane.css("width","20%").fadeIn();
+        flag=false;
+    }else{
+        $("#videoPane").css("width","100%");
+        operatePane.css("width","0").fadeOut();
+        flag=true;
+    }
+    return flag;
+};
 $(function(){
 
     $(".toolBarPane .icon-volume-up").click(function(){
@@ -8,28 +21,28 @@ $(function(){
     });
     var toolBarPane=$(".toolBarPane");
     var flag=true;
-    $("#show").click(function(){
-        if(flag){
-            $("#videoPane").css("width","80%");
-            $(".otherPane").css("width","20%").fadeToggle();
-            flag=false;
-        }else{
-            $("#videoPane").css("width","100%");
-            $(".otherPane").css("width","0").fadeToggle();
-            flag=true;
-        }
-
+    $("#operate1").click(function(){
+        flag=showOperatePane($("#operatePane1"),flag);
     });
-    $("#removePane").click(function(){
+    $("#operate2").click(function(){
+        flag=showOperatePane($("#operatePane2"),flag);
+    });
+    $("#operate3").click(function(){
+        flag=showOperatePane($("#operatePane3"),flag);
+    });
+    $("#operate4").click(function(){
+        flag=showOperatePane($("#operatePane4"),flag);
+    });
+    $(".closePane").click(function(){
         $("#videoPane").css("width","100%");
-        $(".otherPane").css("width","0").fadeToggle();
+        $(this.parentNode).css("width","0").fadeToggle();
         flag=true;
     });
     initPanelHeadingEvt();
 });
 
 /**
- * Created by xuds on 2015/7/11.
+ * Created by xuds on 2015/7/17.
  */
 /**
  * @summary 初始化隐藏标题栏,鼠标移入显示标题栏,延迟n秒后隐藏,移出时隐藏.
@@ -38,7 +51,7 @@ var initPanelHeadingEvt =function(){
     var domNode=$("#videoPane");
     var outTimer;
     domNode.mousemove(function(){
-        var headingPane=$(".toolBarPane,.otherToolBarPane");
+        var headingPane=$(".topToolBarPane,.rightToolBarPane");
         headingPane.css("visibility","visible");
         headingPane.css("display","block");
         clearTimeout(outTimer);
@@ -49,6 +62,6 @@ var initPanelHeadingEvt =function(){
         },2000);
     });
     domNode.mouseout(function(){
-        $(".toolBarPane,.otherToolBarPane").css("visibility","hidden");
+        $(".topToolBarPane,.rightToolBarPane").css("visibility","hidden");
     })
 };
